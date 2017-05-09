c57411dcd0d9bddec4048f0be9fe76ef6a2b0723
imota <mota.icaro@gmail.com>
2017-05-09 19:22:16 -0300

    Plotting 3d graphs as data analysis

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import random
 import pandas as pd
 import numpy as np 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
+from mpl_toolkits.mplot3d import Axes3D
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -23,12 +25,29 @@ def get_train_data():
 	data_train = pd.read_csv('p1_data_train.csv')
 	X = [get_temps_of_line(i, data_train) for i in range(5209)]
 	y = [get_target(i, data_train) for i in range(5209)]
+
+	plot(X,y)
 	return X, y
 
 def print_results(y_test, test_predictions, clf):
 	print(confusion_matrix(y_test,test_predictions))
 	print(classification_report(y_test,test_predictions))
 	print(clf.coef_)
+
+def plot(X,y):
+	Xt = [X[i] for i in range(len(X)) if y[i] == True]
+	Xf = [X[i] for i in range(len(X)) if y[i] == False]
+
+	x0t, x0f = [random.choice(Xt)[0] for i in range(200)], [random.choice(Xf)[0] for i in range(200)]
+	x1t, x1f = [random.choice(Xt)[1] for i in range(200)], [random.choice(Xf)[1] for i in range(200)]
+	x2t, x2f = [random.choice(Xt)[2] for i in range(200)], [random.choice(Xf)[2] for i in range(200)]
+	x3t, x3f = [random.choice(Xt)[3] for i in range(200)], [random.choice(Xf)[3] for i in range(200)]
+
+	fig = plt.figure()
+	ax1 = Axes3D(fig)
+	ax1.plot(x0t, x1t, 'ro', zs = x2t, alpha = 0.5)
+	ax1.plot(x0f, x1f, 'bo', zs = x2f, alpha = 0.5)
+	plt.show()	
 
 def train():
 	X, y = get_train_data()
@@ -37,24 +56,16 @@ def train():
 	test_predictions = clf.predict(X_test)
 	print_results(y_test, test_predictions, clf)
 
-	fig = plt.figure()
-	ax1 = fig.add_subplot(111)
-	#ax1.set_title("Mains power stability")    
-	#ax1.set_xlabel('time')
-	#ax1.set_ylabel('Mains voltage')
-	ax1.plot(X,y, c='r', label='the data')
-	leg = ax1.legend()
-	plt.show()
-
 	return clf
 
 def test():
 	data_test = pd.read_csv('p1_data_test.csv')
+
 	X = [get_temps_of_line(i, data_test) for i in range(5208)]
 	p1_predictions = clf.predict(X)
 	
 	df = pd.DataFrame(p1_predictions, columns = ['target'])
-	df.to_csv('p1_predictions.csv', index = False)
+	df.to_csv('p1_predictions.csv', index = False, quoting = 1)
 
 if __name__ == "__main__":
 	clf = LogisticRegression(class_weight = 'balanced', solver = 'lbfgs')
