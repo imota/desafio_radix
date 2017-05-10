4087b18ea4a8d066d4b11535732cad31229e0ef0
imota <mota.icaro@gmail.com>
2017-05-10 09:05:30 -0300

    Commiting code that draws the results and saves them as png

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.patches as mpatches
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -34,6 +35,23 @@ def print_results(y_test, test_predictions, clf):
 	print(classification_report(y_test,test_predictions))
 	print(clf.coef_)
 
+def saveFigure(xt1, xt2, xt3, xf1, xf2, xf3, x1_name, x2_name, x3_name):
+	fig = plt.figure()
+	ax1 = Axes3D(fig)
+	ax1.plot(linestyle='None', color='red', marker='o', xs=xt1, ys=xt2, zs=xt3, alpha=0.5)
+	ax1.plot(linestyle='None', color='blue', marker='o', xs=xf1, ys=xf2, zs=xf3, alpha=0.5)
+	
+	ax1.set_xlabel(x1_name)
+	ax1.set_ylabel(x2_name)
+	ax1.set_zlabel(x3_name)
+
+	#red_patch = mpatches.Patch(color='red', label='1')
+	#plt.legend(handles=[red_patch])
+	#blue_patch = mpatches.Patch(color='blue', label='0')
+	#plt.legend(handles=[blue_patch])	
+
+	plt.savefig(x1_name + x2_name + x3_name + '.png')
+
 def plot(X,y):
 	Xt = [X[i] for i in range(len(X)) if y[i] == True]
 	Xf = [X[i] for i in range(len(X)) if y[i] == False]
@@ -43,11 +61,10 @@ def plot(X,y):
 	x2t, x2f = [random.choice(Xt)[2] for i in range(200)], [random.choice(Xf)[2] for i in range(200)]
 	x3t, x3f = [random.choice(Xt)[3] for i in range(200)], [random.choice(Xf)[3] for i in range(200)]
 
-	fig = plt.figure()
-	ax1 = Axes3D(fig)
-	ax1.plot(x0t, x1t, 'ro', zs = x2t, alpha = 0.5)
-	ax1.plot(x0f, x1f, 'bo', zs = x2f, alpha = 0.5)
-	plt.show()	
+	saveFigure(x0t, x1t, x2t, x0f, x1f, x2f, 'Temp1', 'Temp2', 'Temp3')
+	saveFigure(x0t, x1t, x3t, x0f, x1f, x3f, 'Temp1', 'Temp2', 'Temp4')
+	saveFigure(x0t, x2t, x3t, x0f, x2f, x3f, 'Temp1', 'Temp3', 'Temp4')
+	saveFigure(x1t, x2t, x3t, x1f, x2f, x3f, 'Temp2', 'Temp3', 'Temp4')
 
 def train():
 	X, y = get_train_data()
